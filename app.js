b562e99d233b7e6bc2325389879a862d70055b7a
Leon Yalin <yelinichevl@gmail.com>
2019-02-20 09:45:20 +0200

    first page

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const chalk = require('chalk');
-const debug = require('debug')
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 
+app.use(morgan('tiny')); // logs network requests
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist')));
+
 app.get('/', (req, res) => {
-    res.send("Hello from Express");
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.post('/', (req, res) => {
+    res.send("Hello from Express:POST");
 });
 
 app.listen(4000, () => {
-    console.log(`App listening on port ${chalk.green('4000')}`);
+    debug(`App listening on port ${chalk.green('4000')}`); // logs in debug mode
 });
