9077e3f54218539c1b43fd09cae96ee258ba7d84
Leon Yalin <leon.yalin@cellebrite.com>
2019-02-24 12:59:07 +0200

    templating engines

--- app.js
+++ app.js
@@ -7,14 +7,28 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 4000;
 
+function prevCode() {
+// function prevCode(app, res) {
+  // app.set('view engine', 'pug');
+  // res.send('Hello from Node!');
+  // res.sendFile(path.join(__dirname, 'views', 'index.html'));
+}
+
 app.use(morgan('tiny')); // logs network requests
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist')));
 
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  prevCode(app, res);
+  res.render('index', {
+    title: 'Hello node!',
+    items: ['first', 'second', 'third'],
+  });
 });
 
 app.post('/', (req, res) => {
